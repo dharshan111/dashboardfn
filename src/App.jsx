229e2d23af065adc396e9b87c821178e04859ab9
dharshan111 <dharshan.rj235@gmail.com>
2026-02-02 19:07:17 -0800

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,12 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { Suspense, lazy } from "react";
-
-// Public pages (eager)
+  
 import Loginpage from "./components/ContactSection/LoginRegister/Loginpage";
 import Registerpage from "./components/ContactSection/LoginRegister/Registerpage";
-
-// Auth wrapper
+import Mainfolder from "./components/Dashboard/MainFolder/Mainfolder";
+import Maincontact from "./components/ContactSection/Maincontact";
 import PrivateRoute from "./PrivateRoute";
-
-// Lazy pages (heavy)
-const Mainfolder = lazy(() =>
-  import("./components/Dashboard/MainFolder/Mainfolder")
-);
-
-const Maincontact = lazy(() =>
-  import("./components/ContactSection/Maincontact")
-);
-
-// Loader component
-const Loader = () => (
-  <div
-    style={{
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: "18px",
-    }}
-  >
-    Loading...
-  </div>
-);
-
 function App() {
   return (
     <Router>
@@ -44,30 +17,25 @@ function App() {
         {/* Default */}
         <Route path="/" element={<Navigate to="/login" />} />
 
-        {/* Public */}
+        {/* Public Routes */}
         <Route path="/login" element={<Loginpage />} />
         <Route path="/register" element={<Registerpage />} />
 
-        {/* Private: Dashboard */}
+        {/* Private Routes */}
         <Route
           path="/dashboard"
           element={
             <PrivateRoute>
-              <Suspense fallback={<Loader />}>
-                <Mainfolder />
-              </Suspense>
+              <Mainfolder />
             </PrivateRoute>
           }
         />
 
-        {/* Private: Contacts */}
         <Route
           path="/contacts"
           element={
             <PrivateRoute>
-              <Suspense fallback={<Loader />}>
-                <Maincontact />
-              </Suspense>
+              <Maincontact />
             </PrivateRoute>
           }
         />
