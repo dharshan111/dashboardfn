562e9b130a212e8574e3059685fe4f62f6c41952
dharshan111 <dharshan.rj235@gmail.com>
2026-02-02 18:58:30 -0800

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,70 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-  
+import { Suspense, lazy } from "react";
+
+// Public pages (load immediately)
 import Loginpage from "./components/ContactSection/LoginRegister/Loginpage";
 import Registerpage from "./components/ContactSection/LoginRegister/Registerpage";
-import Mainfolder from "./components/Dashboard/MainFolder/Mainfolder";
-import Maincontact from "./components/ContactSection/Maincontact";
+
+// Private route wrapper
 import PrivateRoute from "./PrivateRoute";
+
+// Lazy-loaded pages (heavy ones)
+const Mainfolder = lazy(() =>
+  import("./components/Dashboard/MainFolder/Mainfolder")
+);
+
+const Maincontact = lazy(() =>
+  import("./components/ContactSection/Maincontact")
+);
+
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Default */}
-        <Route path="/" element={<Navigate to="/login" />} />
+      <Suspense
+        fallback={
+          <div
+            style={{
+              height: "100vh",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: "18px",
+            }}
+          >
+            Loading dashboard...
+          </div>
+        }
+      >
+        <Routes>
+          {/* Default route */}
+          <Route path="/" element={<Navigate to="/login" />} />
 
-        {/* Public Routes */}
-        <Route path="/login" element={<Loginpage />} />
-        <Route path="/register" element={<Registerpage />} />
+          {/* Public routes */}
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/register" element={<Registerpage />} />
 
-        {/* Private Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Mainfolder />
-            </PrivateRoute>
-          }
-        />
+          {/* Private routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Mainfolder />
+              </PrivateRoute>
+            }
+          />
 
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute>
-              <Maincontact />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute>
+                <Maincontact />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
